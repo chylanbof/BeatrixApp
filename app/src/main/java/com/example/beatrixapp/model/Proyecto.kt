ac9682a9f93569d2cb5410093d508aa0aac04014
WuQiaoq <qwu2324@politecnics.barcelona>
2025-12-04 20:19:49 +0100

    cambiado variable de clase de proyecto

--- app/src/main/java/com/example/beatrixapp/model/Proyecto.kt
+++ app/src/main/java/com/example/beatrixapp/model/Proyecto.kt
@@ -1,8 +1,12 @@
 package com.example.beatrixapp.model
 
-data class Proyecto (
+data class Proyecto(
     val nombreProyecto: String,
+    val descripcionProyecto: String,
     val tareas: List<Tarea>,
     val fechaInicio: String,
-    val fechaEntrega: String //hola
-                    )
+    val fechaEntrega: String,
+    val usuariosAsignados: List<Usuario>,
+    var estado: String = "Pendiente"
+
+)
